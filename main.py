b0e0c2310d53164ef7c23edea8a01ddd260c597a
qseft0402 <qseft0402@gmail.com>
2022-08-13 10:25:23 +0800

    ver.20220813

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("its so goddddddd")
+    print("3 !!!")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
